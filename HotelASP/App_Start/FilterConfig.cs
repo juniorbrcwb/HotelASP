cee5ad770c858371cefa8dcbd675a7817eaa2907
junior <junior@WALDEMARJUN61E9>
2017-04-20 09:04:14 -0300

    Add project files.

---
+++ HotelASP/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HotelASP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ HotelASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HotelASP.MvcApplication" Language="C#" %>
